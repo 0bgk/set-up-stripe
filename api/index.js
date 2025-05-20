be04b7e7284521d723248542a9d4990b1400cfa4
0bgk <orlandohector41@gmail.com>
2025-05-20 12:35:10 +0200

    feat: basic api with express created

---
+++ api/index.js
@@ -0,0 +1,52 @@
+import express from 'express'
+import Stripe from 'stripe'
+import { env } from './env.js';
+
+const app = express()
+const stripe = new Stripe(env.STRIPE_PRIVATE_API_KEY)
+
+app.use(express.json())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*') 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204) 
+  }
+  next()
+})
+
+app.post('/create-checkout-session', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      ui_mode: 'custom',
+      line_items: [
+        {
+          // Provide the exact Price ID (e.g. price_1234) of the product you want to sell
+          price: '{{PRICE_ID}}',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      return_url: `${env.API_URL}/return.html?session_id={CHECKOUT_SESSION_ID}`,
+    })
+
+    res.send({ clientSecret: session.client_secret })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+})
+
+app.get('/session-status', async (req, res) => {
+  const session = await stripe.checkout.sessions.retrieve(req.query.session_id)
+
+  res.send({
+    status: session.status,
+    customer_email: session.customer_details.email
+  })
+})
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
